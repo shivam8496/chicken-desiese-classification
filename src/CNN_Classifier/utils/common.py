e4ddb764f89ee0f4e05dd234a3a24b8576999939
Shivam singh <79458284+shivam8496@users.noreply.github.com>
2025-03-02 21:13:21 +0530

    Created data ingestion pipeline in notebook

--- src/CNN_Classifier/utils/common.py
+++ src/CNN_Classifier/utils/common.py
@@ -7,14 +7,59 @@ from box.exceptions import BoxValueError
 import yaml 
 
 
+
 @ensure_annotations
-def read_yaml(path_to_yaml:Path)->ConfigBox:
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
+    """
     try:
-        with open(path_to_yaml) as file_obj:
-            file_content=yaml.safe_load(file_obj)
-            logger.info(f"yaml file:{path_to_yaml} file Content loaded")
-        return file_content
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
     except BoxValueError:
-        raise ValueError("Cannot Find the file")
+        raise ValueError("yaml file is empty")
     except Exception as e:
         raise e
+
+
+
+
+@ensure_annotations
+def create_directories(path_to_dir:list,verbose=True):
+    for path in path_to_dir:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"created the file at: {path}")
+
+
+# @ensure_annotations
+# def create_directories(path_to_directories: list, verbose=True):
+#     """create list of directories
+
+#     Args:
+#         path_to_directories (list): list of path of directories
+#         ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+#     """
+#     for path in path_to_directories:
+#         os.makedirs(path, exist_ok=True)
+#         if verbose:
+#             logger.info(f"created directory at: {path}")
+
+@ensure_annotations
+def get_size(path:Path) -> str:
+    
+    size=round(os.path.getsize(path)/1024)
+
+    return f"~ size {size}KB"
+    
